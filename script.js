e90682f711e7aa8f42ba7cb58a774125332a0ee0
Ayo-Osota <osotaayomikun@gmail.com>
2022-12-08 04:46:44 +0100

    Hero section animation done

--- script.js
+++ script.js
@@ -21,37 +21,3 @@ closeBtn.addEventListener("click", closeSideMenu);
 //   "./images/product08.jpg",
 //   "./images/product09.jpg"
 // ]
-
-// const heroAnimationContainer = document.getElementById("flip");
-
-// for (x = 0; x < 5; x++) {
-//   const slides = document.createElement("div");
-//   slides.classList.add("hero-slide");
-//   heroAnimationContainer.append(slides);
-// }
-
-// setInterval(heroCarouselSlide, 3000);
-// for (x = 0; x < 10; x++) {
-//     const slides = document.createElement("div");
-//     slides.style.width = "80.8px";
-//     slides.style.height = "100%";
-//     slides.style.backgroundColor = "#d9d9d9";
-//     slides.style.border = "solid blue";
-//     slides.style.position = "absolute";
-//     slides.innerText = x;
-    
-//     for (xi = 5; xi < x; xi++) {
-//       slides.style.backgroundColor = "red";
-//       // slides.style.flexBasis = "0";
-//       // slides.style.order = "1";
-//     }
-
-//     for (xii = 9; xii < x; xii++) {
-//       // slides.style.backgroundColor = "red";
-//       // slides.style.flexBasis = "0";
-//       // slides.style.order = "-1";
-//       // slides.style.transition = "order 10s";
-//     }
-    
-//     animationContainer.append(slides)
-// }
